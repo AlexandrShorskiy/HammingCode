494162b181c82d45b77316538c58664f985f6170
ashorskiy <a.shorskiy@student.csn.khai.edu>
2018-05-09 13:24:56 +0300

    Initial commit

---
+++ CodeHaming/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace CodeHaming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(493, 88);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(648, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(195, 273);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(513, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 88);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Закодировать сообщение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(12, 141);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(493, 88);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сообщение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Двоичное представление сообщения";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox3.Location = new System.Drawing.Point(12, 260);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(198, 88);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox4.Location = new System.Drawing.Point(218, 260);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(287, 88);
+            this.richTextBox4.TabIndex = 7;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox4_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Блоки исходного кода";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Блоки кода, содержащие контрольные биты";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(513, 260);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 72);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Сохранить/\r\nпереслать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(511, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 75);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Проверить и исправить ошибки";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox5.Location = new System.Drawing.Point(12, 379);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.ReadOnly = true;
+            this.richTextBox5.Size = new System.Drawing.Size(240, 88);
+            this.richTextBox5.TabIndex = 12;
+            this.richTextBox5.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox6.Location = new System.Drawing.Point(272, 379);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.ReadOnly = true;
+            this.richTextBox6.Size = new System.Drawing.Size(233, 88);
+            this.richTextBox6.TabIndex = 13;
+            this.richTextBox6.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox7.Location = new System.Drawing.Point(16, 498);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.ReadOnly = true;
+            this.richTextBox7.Size = new System.Drawing.Size(489, 88);
+            this.richTextBox7.TabIndex = 14;
+            this.richTextBox7.Text = "";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(513, 498);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 75);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Декодировать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(648, 333);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.Size = new System.Drawing.Size(195, 253);
+            this.dataGridView2.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 363);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Отправленная последовательность";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(280, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(182, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Коррекция битов (если она нужна)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 482);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Полученное сообщение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(855, 607);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "HammingCode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+
+    }
+}
+
---
+++ CodeHaming/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodeHaming
+{
+    public partial class Form1 : Form
+    {
+        public int r = 4;
+        public int m;
+        public List<string> blocks;
+        public List<string> newBlocks;
+        public Dictionary<char, string> Codes = new Dictionary<char, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m = (int)Math.Pow(2, r) - r - 1;
+            blocks = new List<string>();
+        }
+
+        private void CreateByneryBlocks()
+        {
+            richTextBox2.Text = "";
+            richTextBox3.Text = "";
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Add("Symbol", "Символ");
+            dataGridView1.Columns[0].Width = 60;
+            dataGridView1.Columns.Add("Code", "Код");
+            dataGridView1.Columns[1].Width = 100;
+            foreach (var ch in richTextBox1.Lines)
+            {
+                foreach (var c in ch)
+                {
+                    if (!Codes.Keys.Contains(c))
+                    {
+                        dataGridView1.Rows.Add(c, Convert.ToString(c, 2));
+                        Codes.Add(c, Convert.ToString(c, 2));
+                    }
+                    richTextBox2.Text += Convert.ToString(c, 2);
+                }
+            }
+            int k = 0;
+            string param = "";
+            foreach (var ch in richTextBox2.Lines)
+            {
+                foreach (var c in ch)
+                {
+                    param += c;
+                    if (k == (m - 1)) { blocks.Add(param); k = -1; param = ""; }
+                    k++;
+                }
+            }
+            if (k % (m - 1) != 0) blocks.Add(param);
+            if (blocks[blocks.Count - 1].Length != m)
+                while(m - blocks[blocks.Count - 1].Length!=0)
+                    blocks[blocks.Count - 1] += "0";
+            for (int i = 0; i < blocks.Count; i++)
+                richTextBox3.Text += blocks[i] + "\r\n";
+        }
+        private void SetControlBits()
+        {
+            for (int i = 0; i < blocks.Count; i++)
+            {
+                blocks[i] = blocks[i].Insert(0, "0");
+                blocks[i] = blocks[i].Insert(1, "0");
+                blocks[i] = blocks[i].Insert(3, "0");
+                blocks[i] = blocks[i].Insert(7, "0");
+                int state = 0;
+                for (int j = 0; j < blocks[i].Length; j++)
+                    if (j % 2 == 0) state += Convert.ToInt32(blocks[i][j]);
+                blocks[i] = blocks[i].Remove(0, 1).Insert(0, (state % 2).ToString());
+                state = 0;
+                for (int j = 1; j < blocks[i].Length - 1; j += 4)
+                    state += Convert.ToInt32(blocks[i][j]) + Convert.ToInt32(blocks[i][j + 1]);
+                blocks[i] = blocks[i].Remove(1, 1).Insert(1, (state % 2).ToString());
+                state = 0;
+                for (int j = 3; j < blocks[i].Length; j += 8)
+                    state += Convert.ToInt32(blocks[i][j]) + Convert.ToInt32(blocks[i][j + 1])
+                        + Convert.ToInt32(blocks[i][j + 2]) + Convert.ToInt32(blocks[i][j + 3]);
+                blocks[i] = blocks[i].Remove(3, 1).Insert(3, (state % 2).ToString());
+                state = 0;
+                for (int j = 7; j < blocks[i].Length; j++)
+                    state += Convert.ToInt32(blocks[i][j]);
+                blocks[i] = blocks[i].Remove(7, 1).Insert(7, (state % 2).ToString());
+
+                richTextBox4.Text += blocks[i] + "\r\n";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateByneryBlocks();
+            SetControlBits();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox5.Text = "";
+            newBlocks = new List<string>();
+            foreach (var str in richTextBox4.Lines)
+                richTextBox5.Text += str;
+
+            string param = "";
+            foreach (var ch in richTextBox5.Text)
+            {
+                param += ch;
+                if (param.Length == (int)Math.Pow(2, r) - 1 && param != "")
+                { newBlocks.Add(param); param = ""; }
+            }
+        }
+
+        private void richTextBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar != '1') && (e.KeyChar != '0'))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private string invertor(string str, int numErr)
+        {
+            if (str[numErr] == '1') return str = str.Remove(numErr, 1).Insert(numErr, "0");
+            else return str = str.Remove(numErr, 1).Insert(numErr, "1");
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < newBlocks.Count; i++)
+            {
+                int NumError = 0;
+                int newState = 0;
+                string param = "";
+
+                param = newBlocks[i];
+                for (int j = 0; j < newBlocks[i].Length; j++)
+                    if (j % 2 == 0&& j!=0) newState += Convert.ToInt32(newBlocks[i][j]) % 2;
+                param = param.Remove(0, 1).Insert(0, (newState % 2).ToString());
+                newState = 0;
+                for (int j = 1; j < newBlocks[i].Length - 1; j += 4)
+                {
+                    if (j != 1)
+                        newState += Convert.ToInt32(newBlocks[i][j]) % 2;
+                    newState += Convert.ToInt32(newBlocks[i][j + 1]) % 2;
+                }
+                param = param.Remove(1, 1).Insert(1, (newState % 2).ToString());
+                newState = 0;
+                for (int j = 3; j < newBlocks[i].Length; j += 8)
+                {
+                    if (j != 3)
+                        newState += Convert.ToInt32(newBlocks[i][j]) % 2;
+                    newState += Convert.ToInt32(newBlocks[i][j + 1]) % 2
+                        + Convert.ToInt32(newBlocks[i][j + 2]) % 2 + Convert.ToInt32(newBlocks[i][j + 3]) % 2;
+                }
+                param = param.Remove(3, 1).Insert(3, (newState % 2).ToString());
+                newState = 0;
+                for (int j = 8; j < newBlocks[i].Length; j++)
+                    newState += Convert.ToInt32(newBlocks[i][j]) % 2;
+                param = param.Remove(7, 1).Insert(7, (newState % 2).ToString());
+
+                for (int j = 0; j < param.Length; j++)
+                    if (param[j] != newBlocks[i][j]) NumError += j + 1;
+                if(newBlocks[i]!=blocks[i])
+                if (NumError != 0) newBlocks[i] = invertor(newBlocks[i], NumError - 1);
+
+                richTextBox6.Text += newBlocks[i] + "\r\n";
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Dictionary<string,char> newDict = new Dictionary<string,char>();
+            dataGridView2.Columns.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView2.Columns.Add("column1", "Код");
+            dataGridView2.Columns.Add("column2", "Символ");
+            foreach (var d in Codes)
+            {
+                newDict.Add(d.Value, d.Key);
+                dataGridView2.Rows.Add(d.Value, d.Key);
+            }
+            string str = "";
+            for (int i = 0; i < newBlocks.Count; i++)
+            {
+                newBlocks[i] = newBlocks[i].Remove(7, 1);
+                newBlocks[i] = newBlocks[i].Remove(3, 1);
+                newBlocks[i] = newBlocks[i].Remove(1, 1);
+                newBlocks[i] = newBlocks[i].Remove(0, 1);
+                str += newBlocks[i];
+            }
+            //for (int i = 0; i < blocks.Count; i++)
+            //{
+            //    blocks[i] = blocks[i].Remove(7, 1);
+            //    blocks[i] = blocks[i].Remove(3, 1);
+            //    blocks[i] = blocks[i].Remove(1, 1);
+            //    blocks[i] = blocks[i].Remove(0, 1);
+            //    str += blocks[i];
+            //}
+            string group = "";
+            string result = "";
+            foreach(var ch in str)
+            {
+                group+=ch;
+                if (newDict.ContainsKey(group))
+                {
+                    result += newDict[group];
+                    group = "";
+                }
+                if (group.Length > m) group = "";
+            }
+            richTextBox7.Text = result;
+        }
+
+
+        
+    }
+}
